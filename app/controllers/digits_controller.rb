21bf9f70e193c786c01bdf443f102f8e376a7da3
Neil Garcia <neil.peter.garcia@gmail.com>
2018-09-09 20:14:34 +0800

    index

--- app/controllers/digits_controller.rb
+++ app/controllers/digits_controller.rb
@@ -1,4 +1,13 @@
 class DigitsController < ApplicationController
+  def index
+  end
+
+  def generate
+    key = SecureRandom.hex[0...5]
+
+    return redirect_to digit_path(key)
+  end
+
   def show
     room_result = SubscriberTracker.get_room_results(params[:id])
     @digits = room_result.present? ? room_result.split(',') : [1,2,3,4,5,6]
